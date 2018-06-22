f3eb46b8dd47068ab04260904df8026cef1f7414
Alonso Zhang <code@zhangwu.tech>
2018-06-22 16:10:24 +0800

    登录界面

---
+++ TodayNews/Classes/Mine/Controller/MoreLoginViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MoreLoginViewController.swift
+//  TodayNews
+//
+//  Created by Alonso on 2018/6/22.
+//  Copyright © 2018 Alonso. All rights reserved.
+//
+
+import UIKit
+import IBAnimatable
+
+class MoreLoginViewController: AnimatableModalViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
